1ca70870e828f1ec8703c5c2681b5bbbb9632279
odinellefsen <odin@ellefsen.fo>
2025-09-28 18:58:43 +0100

    Comment out ReactQueryDevtools in QueryProvider component to streamline the development environment.

--- components/providers/query-provider.tsx
+++ components/providers/query-provider.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { queryClient } from '@/lib/query-client'
 
 interface QueryProviderProps {
@@ -12,7 +11,7 @@ export function QueryProvider({ children }: QueryProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   )
 }
