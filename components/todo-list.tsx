7dfa5356e842b7cf389e2cde8f72abdb0cb084d2
odinellefsen <odin@ellefsen.fo>
2026-02-20 20:25:27 +0000

    fix: adjust z-index of Floating Action Buttons in TodoList for proper layering

--- components/todo-list.tsx
+++ components/todo-list.tsx
@@ -462,7 +462,7 @@ export function TodoList() {
       </div>
 
       {/* Floating Action Buttons */}
-      <div className="fixed bottom-6 right-6 z-50 flex flex-col gap-3 animate-scale-in">
+      <div className="fixed bottom-6 right-6 z-40 flex flex-col gap-3 animate-scale-in">
         <SimpleHabitDrawer>
           <Button
             size="lg"
